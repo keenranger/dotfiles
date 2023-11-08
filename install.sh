466b6209dc0c07f726bf6cfca44cee65be841865
Hankyeol Kyung <kghnkl0103@gmail.com>
2023-11-08 18:02:29 +0900

    feat: install container

--- install.sh
+++ install.sh
@@ -28,6 +28,29 @@ set_mac(){
 	brew install --cask rectangle
 }
 
+container(){
+	brew install podman
+	podman machine init
+	podman machine start
+	pipx install podman-compose
+	sudo apt-get update
+	sudo apt install ca-certificates curl gnupg
+	sudo install -m 0755 -d /etc/apt/keyrings
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+	# Add the repository to Apt sources:
+	echo \
+  		"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  		"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+		sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	sudo apt update
+	sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	sudo usermod -aG docker $USER
+	sudo service docker restart
+	# may need to be restarted
+}
+
 if [ $# = 0 ]; then
 	create_symlinks
 	set_zsh
