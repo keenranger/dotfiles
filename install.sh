96648495bcb137f8cb016466ce052e96468885ae
Hankyeol Kyung <kghnkl0103@gmail.com>
2025-05-27 16:44:21 +0900

    Add installation script for uv and update tmux key bindings
    Signed-off-by: Hankyeol Kyung <kghnkl0103@gmail.com>

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ set_zsh(){
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+	/bin/sh -c "$(curl -fsSL https://astral.sh/uv/install.sh)"
 	
 
     type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
